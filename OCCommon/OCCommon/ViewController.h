361da8a22fe401d370cfa825582a32833c3abb9a
yihongmingfeng <2547050802@qq.com>
2016-02-29 23:27:56 +0800

    创建了项目

---
+++ OCCommon/OCCommon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCCommon
+//
+//  Created by 一泓明峰 on 16/2/29.
+//  Copyright © 2016年 RJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCCommon/OCCommon/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCCommon
+//
+//  Created by 一泓明峰 on 16/2/29.
+//  Copyright © 2016年 RJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
